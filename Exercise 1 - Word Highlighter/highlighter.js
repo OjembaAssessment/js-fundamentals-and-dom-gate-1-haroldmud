1121a8dad9bf72567e6b1fbfa8d35aaac2343296
harold <xo@harolds-MacBook-Pro.local>
2023-07-24 11:28:49 +0200

    feat: Got done with the first challenge

--- Exercise 1 - Word Highlighter/highlighter.js
+++ Exercise 1 - Word Highlighter/highlighter.js
@@ -1,1 +1,21 @@
-console.log("Exercise 1 - Word Highlighter");
+let paragraph = document.getElementById('myParagraph');
+const array = paragraph.outerHTML.split(' ');
+const arr = array.filter(item => item !== '');
+
+function mode(arr){
+  return arr.sort((a,b) =>
+        arr.filter(v => v===a).length
+      - arr.filter(v => v===b).length
+  ).pop(); // 'be'
+}
+
+const highlight = array.map(item => {
+  if(item === 'be') {
+    return `<span style="background-color:blue;">${item}</span>`
+  }else{
+    return item
+  }
+})
+
+let newParagraph = highlight.join(' ');
+paragraph.innerHTML = newParagraph;
